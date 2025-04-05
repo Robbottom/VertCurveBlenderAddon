476a1a64710c5a5821401e2ef9bb378827a9d9e6
Robbottom <mrmoggerson@gmail.com>
2025-04-05 13:31:32 -0400

    Added a Screw Modifier Pannel

--- VertCurve.py
+++ VertCurve.py
@@ -19,6 +19,7 @@ class VertCurve(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'VertCurve'
+    bl_options = {'DEFAULT_CLOSED'}  
 
     def draw(self, context):
         layout = self.layout
@@ -28,10 +29,10 @@ class VertCurve(bpy.types.Panel):
         row.operator("mesh.primitive_vertex_add")
         row = layout.row()
         row.label(text="Add A Skin Modifier", icon='MOD_SKIN')
-        row.operator("object.modifier_add").type = 'SKIN'
+        row.operator("object.modifier_add", text="Add A Skin Modifier").type = 'SKIN'
         row = layout.row()
         row.label(text="Add Subdivision", icon='MOD_SUBSURF')
-        row.operator("object.modifier_add").type = 'SUBSURF'
+        row.operator("object.modifier_add", text="Add A SubSurf Modifier").type = 'SUBSURF'
 
 
 class ADD_OT_VERTEX(bpy.types.Operator):
@@ -39,31 +40,49 @@ class ADD_OT_VERTEX(bpy.types.Operator):
     bl_idname = "mesh.primitive_vertex_add"
     
     def execute(self, context):
-        # New mesh object
+       
         mesh = bpy.data.meshes.new(name="NewMesh")
         obj = bpy.data.objects.new("NewObject", mesh)
         
-        # Link object to scene
+     
         bpy.context.collection.objects.link(obj)
         
         bm = bmesh.new()
-        bm.verts.new((0, 0, 0))
+        bm.verts.new((0, 0, 0))  
         bm.verts.ensure_lookup_table()
         bm.to_mesh(mesh)
         bm.free()
 
         return {'FINISHED'}
 
+class AddScrewModifier(bpy.types.Panel):
+    bl_label = "Screw Modifier"
+    bl_idname = "PT_AddScrewModifier"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'VertCurve'
+    bl_parent_id = 'PT_VertPanel'
+    bl_options = {'DEFAULT_CLOSED'}  
 
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.label(text="Add a screw modifier", icon='MOD_SCREW')
+        row.operator("object.modifier_add", text="Add Screw Modifier").type = 'SCREW'
+        
+        
+        
 def register():
     bpy.utils.register_class(VertCurve)
     bpy.utils.register_class(ADD_OT_VERTEX)
+    bpy.utils.register_class(AddScrewModifier)
 
 def unregister():
     bpy.utils.unregister_class(VertCurve)
     bpy.utils.unregister_class(ADD_OT_VERTEX)
+    bpy.utils.unregister_class(AddScrewModifier)
 
 if __name__ == "__main__":
     register()
 
-#Thanks ChatGPT For fixing all of the errors :)
+#Thanks ChatGPT for fixing errors, and creating the add vertex operator :)
